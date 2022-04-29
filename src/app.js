547767d3910e2c8f00b99c035045b05abda0cc25
Prince Odia <princeodia@gmail.com>
2022-04-29 20:59:02 +0100

    Fix view directory error in app

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.set('views', path.join(__dirname, src, views));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-    res.render(index, { title: 'Index' });
+    res.render('index', { title: 'Index' });
 });
 
 app.listen(3000, () => {
