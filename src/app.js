5b18d71f3307e8c6efd683893f9c434f795948f2
Prince Odia <princeodia@gmail.com>
2022-04-30 00:57:58 +0100

    Create the checking and credit routes

--- src/app.js
+++ src/app.js
@@ -27,6 +27,18 @@ app.get('/savings', (req, res, next) => {
     });
 });
 
+app.get('/checking', (req, res, next) => {
+    res.render('account', {
+        account: accounts.checking
+    });
+});
+
+app.get('/credit', (req, res, next) => {
+    res.render('account', {
+        account: accounts.credit
+    });
+});
+
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!');
 })
