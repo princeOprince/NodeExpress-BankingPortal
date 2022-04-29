e790014bc7e70f0b2bfd4a5963dfee3f13102114
Prince Odia <princeodia@gmail.com>
2022-04-29 21:33:23 +0100

    Read account data

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
+const accounts = JSON.parse(accountData);
+
 app.get('/', (req, res, next) => {
     res.render('index', { title: 'Index' });
 });
