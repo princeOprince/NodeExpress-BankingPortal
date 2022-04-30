2f03d0d60ca384e9b6ef529d52ff0ac9cb2ae795
Prince Odia <princeodia@gmail.com>
2022-04-30 11:54:38 +0100

    Add URL encoded middleware in app

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
