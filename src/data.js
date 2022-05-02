532edb881a16e9e5ef5783c229b245a57b5bf190
Prince Odia <princeodia@gmail.com>
2022-05-02 20:59:28 +0100

    Transition user data to data library

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
-const userData = fs.readFileSync('src/json/users.json', 'utf8');
 const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
 const users = JSON.parse(userData);
